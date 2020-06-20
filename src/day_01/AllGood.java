97ce68420f8f4e15a5c5b26a74fbe57f81195363
atiuz90gmailcom <66764785+atiuz90gmailcom@users.noreply.github.com>
2020-06-19 23:29:14 -0400

    Update AllGood.java

--- src/day_01/AllGood.java
+++ src/day_01/AllGood.java
@@ -5,6 +5,10 @@ public class AllGood {
     public static void main(String[] args) {
 
         // I hope everything is GOOD.
+       
+        // how are you today?
+        
+        // this line is added from GitHub
 
 
     }
