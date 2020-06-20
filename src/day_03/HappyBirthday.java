fc41cef87634ca9f8c19bee82cf83d3c5ed5b3c3
atiuz90gmailcom <66764785+atiuz90gmailcom@users.noreply.github.com>
2020-06-20 05:03:10 -0400

    Update HappyBirthday.java

--- src/day_03/HappyBirthday.java
+++ src/day_03/HappyBirthday.java
@@ -5,6 +5,10 @@ public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday B18");
         
+                System.out.println("Happy Birthday B18");
+        // again
+
+        
         // Changes made here in GitHub
     }
 }
