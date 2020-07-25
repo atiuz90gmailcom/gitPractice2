c6a4ce753e315fa6f07aba285bc6d50bf7ef4446
atiuz90gmailcom <66764785+atiuz90gmailcom@users.noreply.github.com>
2020-07-25 01:14:25 -0400

    Update HappyBirthday.java

--- src/day_01/HappyBirthday.java
+++ src/day_01/HappyBirthday.java
@@ -6,7 +6,11 @@ public class HappyBirthday {
         System.out.println("Happy birthday B18");
 
 
-        // this line is added in inteliJ
+               // this line is added in inteliJ
+
+                // this line is added in GitHub
+        System.out.println("Happy birthday more time);
+        
 
 
 
